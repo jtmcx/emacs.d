e074f50e28908d1779938899218346a4c952d442
jtm <noreply@jtm.cx>
2024-01-31 14:38:11 -0500

    Reformatted lang-coq.el

--- lisp/lang-coq.el
+++ lisp/lang-coq.el
@@ -5,18 +5,15 @@
 (use-package company-coq
   :hook coq-mode
   :custom
-  (company-coq-disabled-features
-    '(hello prettify-symbols spinner)))
+  (company-coq-disabled-features '(hello prettify-symbols spinner)))
 
 ;; The Proof General keybindings are fine, but I generally prefer the
 ;; keybindings in coqide (they're shorter). I've replicated the most
 ;; commonly used coqide keybindings here:
 
-(add-hook 'coq-mode-hook
-  (lambda ()	  
-    (local-set-key (kbd "C-<down>")
-		   #'proof-assert-next-command-interactive)
-    (local-set-key (kbd "C-<up>")
-		   #'proof-undo-last-successful-command)
-    (local-set-key (kbd "C-<right>")
-		   #'proof-goto-point)))
+(defun coq-mode-add-keys ()
+  (local-set-key (kbd "C-<down>")  #'proof-assert-next-command-interactive)
+  (local-set-key (kbd "C-<up>")    #'proof-undo-last-successful-command)
+  (local-set-key (kbd "C-<right>") #'proof-goto-point))
+
+(add-hook 'coq-mode-hook #'coq-mode-add-keys)
