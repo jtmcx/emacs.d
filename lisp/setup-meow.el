fdbc186a44ea4857a08d3ff42f3f903225d9d1aa
jtm <noreply@jtm.cx>
2024-03-03 18:37:13 -0500

    Rewrite setup-meow.el, tangle with org-mode

--- lisp/setup-meow.el
+++ lisp/setup-meow.el
@@ -1,91 +1,123 @@
-;; -*- lexical-binding: t; coding: utf-8 -*-
-
 (use-package meow
   :ensure t
   :demand t
-  :config
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-  (meow-motion-overwrite-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
-  (meow-leader-define-key
-   ;; SPC j/k will run the original command in MOTION state.
-   '("j" . "H-j")
-   '("k" . "H-k")
-   ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument)
-   '("/" . meow-keypad-describe-key)
-   '("?" . meow-cheatsheet))
+  :config (setup-meow))
+
+(defun setup-meow ()
+  "Configure meow and enable it globally."
+
+  (meow-setup-indicator)
+
+  (setq meow-expand-hint-remove-delay 2.5)
+
   (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("9" . meow-expand-9)
-   '("8" . meow-expand-8)
-   '("7" . meow-expand-7)
-   '("6" . meow-expand-6)
-   '("5" . meow-expand-5)
-   '("4" . meow-expand-4)
-   '("3" . meow-expand-3)
-   '("2" . meow-expand-2)
-   '("1" . meow-expand-1)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("[" . meow-beginning-of-thing)
-   '("]" . meow-end-of-thing)
+   ;; Inserts
    '("a" . meow-append)
-   '("A" . meow-open-below)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)
-   '("D" . meow-backward-delete)
-   '("e" . meow-next-word)
-   '("E" . meow-next-symbol)
-   '("f" . meow-find)
-   '("g" . meow-cancel-selection)
-   '("G" . meow-grab)
-   '("h" . meow-left)
-   '("H" . meow-left-expand)
    '("i" . meow-insert)
-   '("I" . meow-open-above)
-   '("j" . meow-next)
-   '("J" . meow-next-expand)
-   '("k" . meow-prev)
-   '("K" . meow-prev-expand)
-   '("l" . meow-right)
-   '("L" . meow-right-expand)
-   '("m" . meow-join)
-   '("n" . meow-search)
-   '("o" . meow-block)
-   '("O" . meow-to-block)
-   '("p" . meow-yank)
-   '("q" . meow-quit)
-   '("Q" . meow-goto-line)
-   '("r" . meow-replace)
-   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
+   '("o" . meow-open-below)
+   '("O" . meow-open-above)
+
+   ;; Selects
+   '("b" . meow-back-word)
+   '("w" . meow-next-word)
+   '("W" . meow-mark-word)
+   '("f" . meow-find)
    '("t" . meow-till)
+   '("x" . meow-line)
+   '("z" . meow-pop-selection)
+
+   ;; Edits
+   '("c" . meow-change)
+   '("d" . meow-kill)
+   '("p" . meow-yank)
    '("u" . meow-undo)
    '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)
-   '("w" . meow-mark-word)
-   '("W" . meow-mark-symbol)
-   '("x" . meow-line)
-   '("X" . meow-goto-line)
    '("y" . meow-save)
-   '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   '("'" . repeat)
-   '("<escape>" . ignore))
+
+   ;; Hints
+   '("0" . meow-expand-0)
+   '("1" . meow-expand-1)
+   '("2" . meow-expand-2)
+   '("3" . meow-expand-3)
+   '("4" . meow-expand-4)
+   '("5" . meow-expand-5)
+   '("6" . meow-expand-6)
+   '("7" . meow-expand-7)
+   '("8" . meow-expand-8)
+   '("9" . meow-expand-9)
+   
+   ;; Misc
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("=" . repeat)
+   '("!" . shell-command-on-region)
+   '("G" . meow-grab)
+   '("X" . meow-goto-line)
+   '("<escape>" . meow-cancel-selection))
+
+  (meow-normal-define-key
+   '("S-<left>"  . meow-left-expand)
+   '("S-<down>"  . meow-next-expand)
+   '("S-<up>"    . meow-prev-expand)
+   '("S-<right>" . meow-right-expand))
+
+  (meow-leader-define-key
+   '("!" . shell)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+
+  (meow-normal-define-key
+   '("/" . isearch-forward-regexp)
+   '("%" . anzu-isearch-query-replace-regexp))
+
+  (defun my/meow-search-forward ()
+    (interactive)
+    (if (meow--direction-backward-p)
+        (meow-search -1)
+      (meow-search +1)))
+
+  (defun my/meow-search-backward ()
+    (interactive)
+    (if (meow--direction-forward-p)
+        (meow-search -1)
+      (meow-search +1)))
+
+  (meow-normal-define-key
+   '("n" . my/meow-search-forward)
+   '("N" . my/meow-search-backward))
+
+  (define-key isearch-mode-map (kbd "<up>") 'isearch-ring-retreat)
+  (define-key isearch-mode-map (kbd "<down>") 'isearch-ring-advance)
+
+  (meow-normal-define-key
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("<" . meow-beginning-of-thing)
+   '(">" . meow-end-of-thing))
+
+  (meow-thing-register 'angle
+    '(pair ("<") (">")) '(pair ("<") (">")))
+ 
+  (setq meow-char-thing-table
+   '((?\( . round)
+     (?\[ . square)
+     (?\{ . curly)
+     (?\< . angle)
+     (?\" . string)
+     (?b  . buffer)
+     (?f  . defun)
+     (?l  . line)
+     (?x  . visual-line)
+     (?p  . paragraph)
+     (?w  . window)
+     (?.  . sentence)))
+
+  ;; Rebind universal-argument from C-u to C-v
+  (global-set-key (kbd "C-v") 'universal-argument)
+  (define-key universal-argument-map (kbd "C-u") nil)
+  (define-key universal-argument-map (kbd "C-v") 'universal-argument-more)
+
+  (global-set-key (kbd "C-d") 'scroll-up-command)
+  (global-set-key (kbd "C-u") 'scroll-down-command)
+
   (meow-global-mode 1))
